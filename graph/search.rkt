4dde87c41f9e8fac71723298b3d825a0def5e4ab
Kyle Isom <kyle@cloudflare.com>
2014-06-11 00:17:58 -0700

    Add DFS to search.

--- graph/search.rkt
+++ graph/search.rkt
@@ -3,26 +3,35 @@
 (require "graph.rkt")
 
 (provide bfs
+         dfs
          connected?
          minimise-path)
+
+(define (get-node-map network)
+  (λ (node)
+    (get-node network node)))
 
 (define (build-path nodes)
   (define (path-search nodes path)
     (cond
-      [(empty? nodes) #f]
+      [(empty? nodes) '()]
       [(empty? (rest nodes)) (reverse (cons (first nodes) path))]
       [else
        (if (neighbours? (first nodes) (second nodes))
            (path-search (rest nodes) (cons (first nodes) path))
            (path-search (rest nodes) path))]))
-  (let ([path (path-search (rest nodes) (list (first nodes)))])
-    (if path
-        (minimise-path path)
-        path)))
+  (if (empty? nodes)
+      '()
+      (let ([path (path-search (rest nodes) (list (first nodes)))])
+        (if path
+            (minimise-path path)
+            path))))
 
 (define (minimise-path nodes)
   (define (equivalent-path? path)
-    (and (path? path)
+    (and (not (empty? nodes))
+         (not (empty? path))
+         (path? path)
          (equal? (node-label (first nodes))
                  (node-label (first path)))
          (equal? (node-label (last nodes))
@@ -32,15 +41,17 @@
         path
         (if (empty? (rest nodes))
             (reverse (cons (first nodes) path))
-            (let* ([neighbours (filter (λ (o)
-                                         (neighbours? (first nodes) o))
-                                       (rest nodes))]
-                   [farthest (last neighbours)])
-                   (shorten-path (memf (λ (o)
-                                         (equal? (node-label o)
-                                                 (node-label farthest)))
-                                       (rest nodes))
-                                 (cons (first nodes) path))))))
+            (let ([neighbours (filter (λ (o)
+                                        (neighbours? (first nodes) o))
+                                      (rest nodes))])
+              (if (empty? neighbours)
+                  '()
+                  (let ([farthest (last neighbours)])
+                    (shorten-path (memf (λ (o)
+                                          (equal? (node-label o)
+                                                  (node-label farthest)))
+                                        (rest nodes))
+                                  (cons (first nodes) path))))))))
   (let ([path (shorten-path nodes '())])
     (if (equivalent-path? path)
         path
@@ -55,7 +66,7 @@
       (error end "not found in network."))
     (define (search queue seen path)
       (if (empty? queue)
-          #f
+          '()
           (let ([t (first queue)]
                 [queue (rest queue)])
             (if (equal? t end)
@@ -70,52 +81,50 @@
                           (cons t path)))))))
     (let ([path (search (list start) '() '())])
       (if path
-          (let ([path (map (λ (nd)
-                             (get-node network nd))
-                           path)])
-            (build-path path))
+          (build-path (map (λ (nd) (get-node network nd)) path))
           #f))))
 
-(define (dfs network start-label end-label)
-  (let ([start-node (get-node network start-label)]
-        [end-node   (get-node network end-label)])
+
+(define (dfs network start end)
+  (let ([start-node (get-node network start)]
+        [end-node   (get-node network end)])
     (when (not start-node)
-      (error start-label "not found in network."))
+      (error start "not found in network."))
     (when (not end-node)
-      (error end-label "not found in network."))
-    (define (search s seen)
-      (printf "Search: stack is ~v; seen is ~v~%" s seen)
-      (if (empty? s)
-          #f
-          (let ([v (first s)]
-                [s (rest s)])
-            (if (equal? v end-label)
-                (reverse (cons v s))
-                (if (member v seen)
-                    (search s seen)
-                    (search (set->list
-                             (set-union
-                              (node-edges
-                               (get-node network v))
-                              (list->set (append s (list v)))))
-                            (cons v seen)))))))
-    (let ([path (search (list start-label) '())])
-      (if (path? (map (λ (nd)
-                        (get-node network nd))
-                      path))
-          path
-          (error "Not a path:" path)))))
+      (error end "not found in network."))
+    (define (search stack seen path)
+      (if (empty? stack)
+          '()
+          (let ([i (first stack)]
+                [stack (rest stack)])
+            (if (equal? i end)
+                (cons i path)
+                (search (append 
+                         (filter-not (λ (edge)
+                                       (member edge seen))
+                                     (set->list
+                                      (node-edges
+                                       (get-node network i))))
+                         stack)
+                        (cons i seen)
+                        (cons i path))))))
+    (let ([path (search (list start) '() '())])
+      (let ([path (map (get-node-map network) (reverse path))])
+        (build-path path)))))
+
+(define (node-connected? network node)
+  (andmap (λ (o)
+            (not (empty? (bfs network (node-label node) (node-label o)))))
+          (filter-not (λ (o)
+                        (equal? (node-label o)
+                                (node-label node)))
+                      network)))
 
 
 (define (connected? network)
   (foldl (λ (x y)
            (and x y))
          #t
-         (map (λ (nd)
-                (andmap (λ (o)
-                          (bfs network (node-label nd) (node-label o)))
-                        (filter-not (λ (o)
-                                      (equal? (node-label o)
-                                              (node-label nd)))
-                                    network)))
+         (map (λ (node)
+                (node-connected? network node))
               network)))
